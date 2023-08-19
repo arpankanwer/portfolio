562cea236cf630578fb7b3b71386da584e637ed1
arpankanwer <arpankanwer98@gmail.com>
2023-08-19 06:09:53 -0400

    New Flutter Update

--- lib/app/sections/portfolio/widgets/project_card.dart
+++ lib/app/sections/portfolio/widgets/project_card.dart
@@ -37,8 +37,8 @@ class ProjectCardState extends State<ProjectCard> {
       },
       child: Container(
         margin: EdgeInsets.symmetric(horizontal: 1.w),
-        width: Responsive.isDesktop(context) ? 70.w : 75.w,
-        height: Responsive.isMobile(context) ? 50.h : 25.h,
+        width: Responsive.isDesktop(context) ? 35.w : 75.w,
+        height: Responsive.isTablet(context) ? 80.h : 50.h,
         decoration: BoxDecoration(
           gradient: isHover ? pinkpurple : grayBack,
           borderRadius: BorderRadius.circular(10),
