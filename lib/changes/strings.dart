096e797e88b0a285d47e4e60299d28f4dcf3db6a
arpankanwer <arpankanwer98@gmail.com>
2024-12-22 12:03:32 -0500

    Update portfolio description and enhance service details in strings.dart

--- lib/changes/strings.dart
+++ lib/changes/strings.dart
@@ -10,19 +10,19 @@ String animationtxt3 = "Program Developer";
 String contactHeadding = "Let’s try my service now!";
 String contactSubHeadding =
     "Let’s work together and make everything super cute and super useful.";
+
 //
 String miniDescription =
     "A passionate individual in the software field is driven by a deep love for creating innovative solutions.";
 
 String servicesSubHeading =
-    "My proficiency lies in a range of programming languages, including Python, Java, Dart, C/C# and JavaScript, which also includs framework like .Net, Spring Boot and Flutter as well. I am adept at web development, employing HTML, CSS, and JavaScript to create dynamic and engaging user interfaces.";
+    "My proficiency lies in a range of programming languages, including Python, Java, Dart, C/C#, and JavaScript, along with frameworks such as .Net, Spring Boot, Flutter, and React Native with Expo. I am adept at web development, employing HTML, CSS, JavaScript, and ReactJS to create dynamic and engaging user interfaces.";
 
 //
-
 String protfolioSubHeading =
     "Since the beginning of my journey as a designer and developer, I have created digital products for business and consumer use. This is a little bit.";
 const String aboutMeHeadline =
-    'I\'m Arpan Kanwer, a Mobile App developer, Web developer and UI designer.';
+    'I\'m Arpan Kanwer, a Mobile App developer, Web developer, and UI designer.';
 
 const String aboutMeDetail =
-    "👋 Greetings! I'm Birarpanjot Singh Kanwer, a dedicated programmer who transforms copious amounts of caffeine ☕ into meticulously crafted lines of code 💻. Welcome to my tech-tastic portfolio, where I merge my lifelong passion for coding with a newfound admiration for the elegance of iOS and Android development using Flutter. 🌟🚀 I'm a currently a student in Sherdan College and pursuing advance diploma in Software developments and Network Engineering. I have  developed several mobile apps which have been publish on play store too. I have also worked as full time tutor of Applied Computing in Sheridan College from January, 2023 to April, 2023 and currently working as an part time tutor in Sheridan College in Applied Computing.";
+    "👋 Greetings! I'm Birarpanjot Singh Kanwer, a dedicated programmer who transforms copious amounts of caffeine ☕ into meticulously crafted lines of code 💻. Welcome to my tech-tastic portfolio, where I merge my lifelong passion for coding with a newfound admiration for the elegance of iOS and Android development using Flutter and React Native with Expo. 🌟🚀 I have completed my three-year advanced diploma in Software Development and Network Engineering from Sheridan College. I have developed several mobile apps, including those published on the Play Store. I worked at Evertz Microsystems as a Software QA Engineer during Winter 2024. Recently, I contributed to a capstone project using ReactJS and React Native with Expo, which won accolades for its innovation and execution.";
