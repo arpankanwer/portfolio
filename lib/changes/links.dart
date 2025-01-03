dc8fccf3bf06a08e03200a0522c3b0cd620b3282
arpankanwer <arpankanwer98@gmail.com>
2025-01-03 01:22:40 -0500

    Update resume link in links.dart to new URL

--- lib/changes/links.dart
+++ lib/changes/links.dart
@@ -1,6 +1,6 @@
 // CV
 const String resume =
-    'https://drive.google.com/file/d/1RtCZ0qQxTQ-BWn_Em4lUj9ctacjueZaF/view?usp=sharing';
+    'https://drive.google.com/file/d/1u-_i8Wm-DwtMuVBVbrBBGyXgYuYIsrii/view?usp=sharing';
 
 // Github Link
 const String gitHub = 'https://github.com/arpankanwer';
