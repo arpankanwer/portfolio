0e7c2f766d6f5249fd385a8489675950641cb99b
arpankanwer <kanwer@sheridancollege.ca>
2025-01-13 16:24:14 -0500

    Add padding to Work section for mobile layout and adjust desktop padding

--- lib/app/sections/work/work.dart
+++ lib/app/sections/work/work.dart
@@ -2,14 +2,18 @@ import 'package:flutter/material.dart';
 import 'package:mysite/app/sections/work/work_desktop.dart';
 import 'package:mysite/app/sections/work/work_mobile.dart';
 import 'package:mysite/core/res/responsive.dart';
+import 'package:sizer/sizer.dart';
 
 class Work extends StatelessWidget {
   const Work({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Responsive(
-      mobile: WorkMobile(),
+    return Responsive(
+      mobile: Padding(
+        padding: EdgeInsets.only(top: 4.w),
+        child: WorkMobile(),
+      ),
       tablet: WorkDesktop(),
       desktop: WorkDesktop(),
     );
