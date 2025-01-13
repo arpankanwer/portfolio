bbcb0a85022737c6972b312a11954ef7633e8cf6
arpankanwer <kanwer@sheridancollege.ca>
2025-01-13 16:23:40 -0500

    Refactor mobile layout for Home section: adjust padding, spacing, and height for improved UI consistency

--- lib/app/sections/home/home_desktop.dart
+++ lib/app/sections/home/home_desktop.dart
@@ -20,76 +20,82 @@ class HomeDesktop extends StatelessWidget {
     var theme = Theme.of(context);
 
     return SizedBox(
-      height: 80.h,
       child: Padding(
-        padding: EdgeInsets.symmetric(horizontal: 10.w),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        padding: EdgeInsets.symmetric(horizontal: 10.w, vertical: 7.w),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            Container(
-              margin: EdgeInsets.only(top: 10.h),
-              width: 55.w,
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Row(
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Container(
+                  margin: EdgeInsets.only(top: 10.h),
+                  width: 55.w,
+                  child: Column(
                     mainAxisSize: MainAxisSize.min,
-                    crossAxisAlignment: CrossAxisAlignment.end,
+                    crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
-                      Text(hellotag,
+                      Row(
+                        mainAxisSize: MainAxisSize.min,
+                        crossAxisAlignment: CrossAxisAlignment.end,
+                        children: [
+                          Text(hellotag,
+                              style: const TextStyle(
+                                fontSize: 25,
+                                fontWeight: FontWeight.w100,
+                              )),
+                          EntranceFader(
+                            offset: const Offset(0, 0),
+                            delay: const Duration(seconds: 2),
+                            duration: const Duration(milliseconds: 100),
+                            child: Image.asset(StaticImage.hi, height: 40),
+                          ),
+                        ],
+                      ),
+                      Space.y(0.5.w)!,
+                      Text(yourname,
                           style: const TextStyle(
-                            fontSize: 25,
-                            fontWeight: FontWeight.w100,
+                            fontSize: 50,
+                            fontWeight: FontWeight.w600,
                           )),
-                      EntranceFader(
-                        offset: const Offset(0, 0),
-                        delay: const Duration(seconds: 2),
-                        duration: const Duration(milliseconds: 800),
-                        child: Image.asset(StaticImage.hi, height: 40),
+                      Row(
+                        crossAxisAlignment: CrossAxisAlignment.end,
+                        children: [
+                          AnimatedTextKit(
+                            isRepeatingAnimation: true,
+                            repeatForever: true,
+                            animatedTexts: desktopList,
+                          ),
+                        ],
+                      ),
+                      Space.y(1.5.w)!,
+                      Padding(
+                        padding: EdgeInsets.only(right: 10.w),
+                        child: Text(miniDescription,
+                            style: TextStyle(
+                              fontSize: isFontSize(context, 20),
+                              fontWeight: FontWeight.w400,
+                              color: theme.textColor
+                                  .withAlpha((0.6 * 255).toInt()),
+                            )),
+                      ),
+                      Space.y(3.w)!,
+                      ColorChageButton(
+                        text: 'download cv',
+                        onTap: () async {
+                          final Uri resumeUrl = Uri.parse(resume);
+                          if (await canLaunchUrl(resumeUrl)) {
+                            await launchUrl(resumeUrl);
+                          }
+                        },
                       ),
                     ],
                   ),
-                  Space.y(0.5.w)!,
-                  Text(yourname,
-                      style: const TextStyle(
-                        fontSize: 50,
-                        fontWeight: FontWeight.w600,
-                      )),
-                  Row(
-                    crossAxisAlignment: CrossAxisAlignment.end,
-                    children: [
-                      AnimatedTextKit(
-                        isRepeatingAnimation: true,
-                        repeatForever: true,
-                        animatedTexts: desktopList,
-                      ),
-                    ],
-                  ),
-                  Space.y(1.5.w)!,
-                  Padding(
-                    padding: EdgeInsets.only(right: 10.w),
-                    child: Text(miniDescription,
-                        style: TextStyle(
-                          fontSize: isFontSize(context, 20),
-                          fontWeight: FontWeight.w400,
-                          color: theme.textColor.withAlpha((0.6 * 255).toInt()),
-                        )),
-                  ),
-                  Space.y(3.w)!,
-                  ColorChageButton(
-                    text: 'download cv',
-                    onTap: () async {
-                      final Uri resumeUrl = Uri.parse(resume);
-                      if (await canLaunchUrl(resumeUrl)) {
-                        await launchUrl(resumeUrl);
-                      }
-                    },
-                  ),
-                ],
-              ),
+                ),
+                const ZoomAnimations(),
+              ],
             ),
-            const ZoomAnimations(),
           ],
         ),
       ),
